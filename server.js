5e5cc23ffea06d910b072c9b6aceaab87f45a0f1
shafa45 <safaullah14@gmail.com>
2022-06-05 14:57:12 +0530

    DB Connection

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ import cors from 'cors';
 const morgan = require('morgan');
 require('dotenv').config();
 import { readdirSync } from 'fs';
+import mongoose from 'mongoose';
 
 // create express app
 const app = express();
+
+// DB Config
+mongoose
+  .connect(process.env.DB_CONNECTION)
+  .then(() => console.log('DB Connected'))
+  .catch((err) => console.log(err));
 
 // apply middlewares
 app.use(cors());
