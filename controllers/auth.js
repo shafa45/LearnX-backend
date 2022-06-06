72ba9d9ae5d095aaf8f58db391a89c2db511c72d
shafa45 <safaullah14@gmail.com>
2022-06-06 21:15:34 +0530

    Send Email with AWS SES

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,16 @@
 import User from '../models/user';
 import { hashPassword, comparePassword } from '../utils/auth';
 import jwt from 'jsonwebtoken';
+import AWS from 'aws-sdk';
+
+const awsConfig = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+  apiVersion: process.env.AWS_API_VERSION,
+};
+
+const SES = new AWS.SES(awsConfig);
 
 export const register = async (req, res) => {
   try {
@@ -113,4 +123,45 @@ export const currentUser = async (req, res) => {
   } catch (err) {
     console.log(err);
   }
+};
+
+export const sendEmail = async (req, res) => {
+  const params = {
+    Source: process.env.EMAIL_FROM,
+    Destination: {
+      ToAddresses: ['safaullah16@gmail.com', 'safaullah14@gmail.com'],
+    },
+    ReplyToAddresses: [process.env.EMAIL_FROM],
+    Message: {
+      Subject: {
+        Data: 'Reset Password',
+        Charset: 'UTF-8',
+      },
+      Body: {
+        Html: {
+          Data: `<html>
+            <body>
+            <h1>Password Reset Link</h1>
+            <p>Please use the following link to reset your password</p>
+            </body>
+            </html>`,
+          Charset: 'UTF-8',
+        },
+      },
+    },
+  };
+  const emailSent = SES.sendEmail(params).promise();
+  emailSent
+    .then((data) => {
+      console.log(data);
+      res.status(200).json({
+        message: 'Email sent successfully',
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json({
+        message: 'Error sending email',
+      });
+    });
 };
--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,18 @@ const router = express.Router();
 // middleware
 import { requireSignin } from '../middlewares';
 
-import { register, login, logout, currentUser } from '../controllers/auth';
+import {
+  register,
+  login,
+  logout,
+  currentUser,
+  sendEmail,
+} from '../controllers/auth';
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/logout', logout);
 router.get('/current-user', requireSignin, currentUser);
+router.get('/send-email', sendEmail);
 
 module.exports = router;
